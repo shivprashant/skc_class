0a1c94b28e182f396fcfabf6bb7b9cd274d98b8d
Shiv <shivprashant@gmail.com>
2018-06-09 09:45:32 -0700

    my first commit

---
+++ loops.py
@@ -0,0 +1,8 @@
+
+#The roll number is our class range from 1 to 100. 
+#The math teacher plans to distribute candies as square of your roll numbers
+# Print squared numbers till 100
+
+for i in range(1,100):
+	print(i, " quared(using for) is ", i*i)
+
